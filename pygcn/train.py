1c2bdc9c918fd9c1bf34bb9219db0300b7ff379f
sqlkrad <56990007+sqlkrad@users.noreply.github.com>
2020-01-12 10:07:02 +0800

    Update train.py

--- pygcn/train.py
+++ pygcn/train.py
@@ -9,8 +9,8 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-from pygcn.utils import load_data, accuracy
-from pygcn.models import GCN
+from utils import load_data, accuracy
+from models import GCN
 
 """
 通过命令行传入的一些参数配置
