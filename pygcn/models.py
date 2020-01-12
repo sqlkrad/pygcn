929449eb46ae2f93715ba8f0133d25b711c3e230
sqlkrad <56990007+sqlkrad@users.noreply.github.com>
2020-01-12 11:54:59 +0800

    Update models.py

--- pygcn/models.py
+++ pygcn/models.py
@@ -14,5 +14,6 @@ class GCN(nn.Module):
     def forward(self, x, adj):
         x = F.dropout(x, self.dropout, training=self.training)
         x = F.relu(self.gc1(x, adj))
+        x = F.dropout(x, self.dropout, training=self.training)
         x = self.gc2(x, adj)
         return F.log_softmax(x, dim=1)
